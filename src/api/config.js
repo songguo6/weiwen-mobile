aa111eb6e904d520eaa5bf088c66d6bcc63b6e6a
songguo6 <zhanglisong25@163.com>
2019-08-05 22:16:46 +0800

    ual

--- src/api/config.js
+++ src/api/config.js
@@ -4,22 +4,12 @@ import { JsSignatureProvider } from 'eosjs/dist/eosjs-jssig';
 const appName = '微文';
 const contract = 'weiwendapps2';
 
-// jungle testnet
-// const network = {
-//   blockchain: 'eos',
-//   protocol: 'https',
-//   host: 'api.jungle.alohaeos.com',
-//   port: 443,
-//   chainId: 'e70aaab8997e1dfce58fbfac80cbbb8fecec7b99cf982a9444273cbc64c41473',
-// };
-
-// mainnet
 const network = {
   blockchain: 'eos',
   protocol: 'https',
-  host: 'api.eosnewyork.io',
-  port: 80,
-  chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
+  host: 'api.jungle.alohaeos.com',
+  port: 443,
+  chainId: 'e70aaab8997e1dfce58fbfac80cbbb8fecec7b99cf982a9444273cbc64c41473',
 };
 
 const signatureProvider = new JsSignatureProvider(['5JtUScZK2XEp3g9gh7F8bwtPTRAkASmNrrftmx4AxDKD5K4zDnr']);

0c3fd8349b6166fcb4e0aeb7532da4a6d0596700
songguo6 <zhanglisong25@163.com>
2019-08-05 20:00:20 +0800

    ual

--- src/index.js
+++ src/index.js
@@ -2,4 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { UALProvider } from 'ual-reactjs-renderer';
+import { TokenPocket } from 'ual-token-pocket';
+
+import { appName, network } from './api/config';
+
+const chain = {
+  chainId: network.chainId,
+  rpcEndpoints: [
+    {
+      protocol: network.protocol,
+      host: network.host,
+      port: network.port,
+    },
+  ],
+}
+
+const supportedChains = [chain];
+const supportedAuthenticators = [new TokenPocket([chain])];
+
+ReactDOM.render(
+  <UALProvider chains={supportedChains} authenticators={supportedAuthenticators} appName={appName}>
+      <App />
+  </UALProvider>, 
+  document.getElementById('root')
+);
--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,16 +1,31 @@
 import React, { Component, Fragment } from 'react';
-import { NavBar } from 'antd-mobile';
+import { NavBar, Button } from 'antd-mobile';
+
+import { withUAL } from 'ual-reactjs-renderer';
 
 class MyPage extends Component {
 
+  displayLoginModal = (display) => {
+    const {ual: {showModal, hideModal}} = this.props;
+    if(display){
+      showModal();
+    }else{
+      hideModal();
+    }
+  }
+
+  login = () => this.displayLoginModal(true);
+
   render() {
+    const {ual: { logout }} = this.props;
     return (
       <Fragment>
         <NavBar mode='dark'>我的</NavBar>
-        我的
+        <Button onClick={this.login}>Login</Button>
+        <Button onClick={logout}>Logout</Button>
       </Fragment>
     )
   }
 }
 
-export default MyPage;
+export default withUAL(MyPage);

ee8ae564e1380177cfc706f5f8912e66be3d07f8
songguo6 <zhanglisong25@163.com>
2019-08-05 22:34:45 +0800

    Updates

---
+++ precache-manifest.db5fe3fdb1353e6304209d00dd15bd23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "596692b63ab31019e217a4ed77ec27eb",
+    "url": "/weiwen-mobile/index.html"
+  },
+  {
+    "revision": "c51bc4618a60226adb04",
+    "url": "/weiwen-mobile/static/css/2.41310215.chunk.css"
+  },
+  {
+    "revision": "390460273f79a3e4889c",
+    "url": "/weiwen-mobile/static/css/main.0f692900.chunk.css"
+  },
+  {
+    "revision": "c51bc4618a60226adb04",
+    "url": "/weiwen-mobile/static/js/2.5781ebd0.chunk.js"
+  },
+  {
+    "revision": "390460273f79a3e4889c",
+    "url": "/weiwen-mobile/static/js/main.a6e25c4b.chunk.js"
+  },
+  {
+    "revision": "e0f9ca5d760f95c8bddd",
+    "url": "/weiwen-mobile/static/js/runtime~main.8524ee8f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weiwen-mobile/precache-manifest.c217a35a3f1212a5fe9d591b17edf4a4.js"
+  "/weiwen-mobile/precache-manifest.db5fe3fdb1353e6304209d00dd15bd23.js"
 );
 
 self.addEventListener('message', (event) => {

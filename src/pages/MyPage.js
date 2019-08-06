a068d99fffb6154185ad6ad557b26afb56fc3891
songguo6 <zhanglisong25@163.com>
2019-08-06 23:51:02 +0800

    tp

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -6,42 +6,26 @@ const tp = require('tp-eosjs');
 
 class MyPage extends Component {
 
-  state = {
-    balance: '',
-    account: '',
-  }
+  state = { balance: '' }
 
   constructor(props){
     super(props);
-    this.onBtn1Click = this.onBtn1Click.bind(this);
-    this.onBtn2Click = this.onBtn2Click.bind(this);
+    this.onBtnClick = this.onBtnClick.bind(this);
   }
 
-  checkConnect(){
+  onBtnClick(){
     if(!tp.isConnected()){
       toast('未连接到 TokenPocket');
     }
-  }
-
-  onBtn1Click(){
-    this.checkConnect();
 
     tp.getEosBalance({
       account: 'songguo12345',
       contract: 'eosio.token',
-      symbol: 'EOS'
+      symbol: 'EOS',
     }).then(res => {
-      this.setState({balance: res.data});
-    });
-  }
-
-  onBtn2Click(){
-    this.checkConnect();
-
-    tp.getEosAccountInfo({
-      account: 'itokenpocket'
-    }).then(res => {
-      this.setState({account: res.data});
+      this.setState({balance: res.data.balance});
+    }).catch(error => {
+      toast(error);
     });
   }
 
@@ -49,14 +33,9 @@ class MyPage extends Component {
     return (
       <Fragment>
         <NavBar mode='dark'>我的</NavBar>
-
-        <Button onClick={this.onBtn1Click}>getEosBalance</Button>
         <WhiteSpace size='lg'/>
+        <Button onClick={this.onBtnClick}>getEosBalance</Button>
         <div>{this.state.balance}</div>
-
-        <Button onClick={this.onBtn2Click}>getEosAccountInfo</Button>
-        <WhiteSpace size='lg'/>
-        <div>{this.state.account}</div>
       </Fragment>
     )
   }
